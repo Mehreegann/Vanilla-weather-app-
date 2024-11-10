93ab8f36748efcff9561ead1d1e02e3689b7733a
Mehreegann <Mehreganyagh@gmail.com>
2024-11-10 23:59:10 +0330

    fixed the js problem

--- src/index.js
+++ src/index.js
@@ -1,23 +1,9 @@
-function updateWeather(response) {
-  let temperatureElement = document.querySelector("#tempreture");
-  let temperature = response.data.temperature.current;
-  let cityElement = document.querySelector("themainheader");
-  cityElement.innerHTML = response.date.city;
-  temperatureElement.innerHTML = Math.round(temperature);
-}
-
-function searchCity(city) {
-  let apikey = "2ee514187aa05b3a9e6c3f3oefdaf2ta";
-  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key={api}`;
-  axios.get(apiUrl).then(updateWeather);
-  //make api call and update the interface
-}
-
-function handelSearchSubmit(event) {
+function handleSearchSubmit(event) {
   event.preventDefault();
-  let searchinput = document.querySelector("#searchfforminput");
-  searchCity(searchinput.value);
+  let searchInput = document.querySelector("#searchf-form-input");
+  let cityElement = document.querySelector("#city");
+  cityElement.innerHTML = searchInput.value;
 }
 
-let searchForElement = document.querySelector("#searchfform");
-searchForElement.addEventListener("submit", handelSearchSubmit);
+let searchFormElement = document.querySelector("#search-form");
+searchFormElement.addEventListener("submit", handleSearchSubmit);
