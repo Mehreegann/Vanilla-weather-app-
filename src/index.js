3c8bedaaa6712c3ad23344ab12a96efcc16a23ba
Mehreegann <Mehreganyagh@gmail.com>
2024-11-14 17:47:32 +0330

    corrected the mistakes with axios

--- src/index.js
+++ src/index.js
@@ -1,8 +1,24 @@
+function refreshWeather(response) {
+  let temperatureElement = document.querySelector("#temperature");
+  let temperature = response.data.temperature.current;
+  let cityElement = document.querySelector("#city");
+  cityElement.innerHTML = response.data.city;
+  temperatureElement.innerHTML = Math.round(temperature);
+  //console.log(response.data.temperature.current);
+}
+
+function searchCity(city) {
+  let apiKey = "2ee514187aa05b3a9e6c3f3oefdaf2ta";
+  let apiURL = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+  axios.get(apiURL).then(refreshWeather);
+}
+
 function handleSearchSubmit(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#searchf-form-input");
-  let cityElement = document.querySelector("#city");
-  cityElement.innerHTML = searchInput.value;
+  //let cityElement = document.querySelector("#city");
+  //cityElement.innerHTML = searchInput.value;
+  searchCity(searchInput.value);
 }
 
 let searchFormElement = document.querySelector("#search-form");
